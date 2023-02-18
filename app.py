1504993cf6b0db73d22d8f6f065abe65cfd9a1e2
GavinKaiHuang <gavinkaihuang@gmail.com>
2023-02-19 01:34:22 +0800

    test: use sqlite as db in flask project

---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import config
+import click
+from flask import Flask
+from flask import redirect, url_for, abort, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+# # SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+app.config.from_object(config)
+
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+path = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, app.config['DB_NAME']))
+app.config['SQLALCHEMY_DATABASE_URI'] = path
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    # path = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, app.config['DB_NAME']))
+    return "Hello %s" %path
+    # return "Hello  " 
+
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0',port=5000)
+    app.run()
---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+ENV = 'development'
+
+DEBUG = True
+
+SECRET_KEY = 'mysecretkey'
+
+DB_NAME = 'sqlite_sent.db'
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///myapp.db'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
