425dac957db278bf6f7534f79255f228f4d44d41
GavinKaiHuang <gavinkaihuang@gmail.com>
2023-02-22 22:26:46 +0800

    update: change mysql connection to nas

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # fsent
 flask project
 
-1, create database
+1, checkout venv
+on mac
+virtualenv --no-site-packages venv
+
+on Linux:
+virtualenv venv
+
+source venv/bin/active
+
+
+2, install requirements
+pip3 install -r requirements.txt
+
+
+3, change database
+use mysql:
+vi config.py
+
+use sqlite:
+
+4, create tables
 cd project_root_folder
-python db_create_user.py
+python3 db_creater.py
 
--- config.py
+++ config.py
@@ -14,14 +14,14 @@ SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
 
 
 # MySQL所在主机名
-HOSTNAME = "127.0.0.1"
+HOSTNAME = "192.168.2.214"
 # MySQL监听的端口号，默认3306
 PORT = 3306
 # 连接MySQL的用户名，自己设置
 USERNAME = "root"
 # 连接MySQL的密码，自己设置
-PASSWORD = "123456"
+PASSWORD = "commonuse"
 # MySQL上创建的数据库名称
-DATABASE = "datacheck"
+DATABASE = "fsentdb"
 # 通过修改以下代码来操作不同的SQL比写原生SQL简单很多 --》通过ORM可以实现从底层更改使用的SQL
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
